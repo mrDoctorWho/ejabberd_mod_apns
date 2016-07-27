3985910159e3999cb62b744043d26d0cd89cc918
John Smith <mrdoctorwho@gmail.com>
2016-07-27 15:03:27 +0700

    Change xml to fastxml & fix #7 — now records should be updated

--- src/mod_apns.erl
+++ src/mod_apns.erl
@@ -75,7 +75,7 @@ add_quotes(String) ->
 
 
 message(From, To, Packet) ->
-	Type = xml:get_tag_attr_s(<<"type">>, Packet),
+	Type = fxml:get_tag_attr_s(<<"type">>, Packet),
 	?DEBUG("Offline message", []),
 	case Type of 
 		"normal" -> ok;
@@ -85,7 +85,7 @@ message(From, To, Packet) ->
 			JTo = jlib:jid_to_string(To#jid{user = To#jid.user, server = To#jid.server, resource = <<"">>}),
 			ToUser = To#jid.user,
 			ToServer = To#jid.server,
-			Body = xml:get_path_s(Packet, [{elem, <<"body">>}, cdata]),
+			Body = fxml:get_path_s(Packet, [{elem, <<"body">>}, cdata]),
 
 			%% Checking subscription
 			{Subscription, _Groups} = 
@@ -122,7 +122,7 @@ iq(#jid{user = User, server = Server}, _, #iq{sub_el = SubEl} = IQ) ->
 	{MegaSecs, Secs, _MicroSecs} = erlang:timestamp(),
 	TimeStamp = MegaSecs * 1000000 + Secs,
 
-	Token = xml:get_tag_cdata(xml:get_subtag(SubEl, <<"token">>)),
+	Token = fxml:get_tag_cdata(fxml:get_subtag(SubEl, <<"token">>)),
 
 	F = fun() -> mnesia:write(#apns_users{user={LUser, LServer}, token=Token, last_seen=TimeStamp}) end,
 
@@ -136,8 +136,8 @@ iq(#jid{user = User, server = Server}, _, #iq{sub_el = SubEl} = IQ) ->
 			mnesia:transaction(F),
 			?DEBUG("mod_apns: Updating last_seen for user ~s@~s", [LUser, LServer]);
 
-		%% Record for this key has been found, but for another key
-		[#apns_users{user={LUser, LServer}, token=Token}] ->
+		%% Record for this key has been found, but with another token
+		[#apns_users{user={LUser, LServer}, token=_}] ->
 			mnesia:transaction(F),
 			?DEBUG("mod_apns: Updating token for user ~s@~s", [LUser, LServer])
 		end,
